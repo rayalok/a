5b8e845398f86cdcf38e0246c8546b093ae4339d
Alok ray <93648878+rayalok@users.noreply.github.com>
2022-02-12 21:25:43 +0600

    Add files via upload

---
+++ alok.js
@@ -0,0 +1,8 @@
+let avItem = document.getElementById("av-item");
+function navHide() {
+    if(avItem.style.display==='block'){
+        avItem.style.display='none';
+    }else{
+        avItem.style.display = 'block';
+    }
+}
